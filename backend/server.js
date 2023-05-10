c6d06c2e0814ec5d690cd986db2ec5a0447b8a43
faslu007 <faslu007@gmail.com>
2023-05-10 09:47:04 +0530

    glitch fix

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const connectDb = require('./config/db')
 const dotenv = require('dotenv').config();
+const path = require('path');
 
 const Service = require('./models/service')
 
