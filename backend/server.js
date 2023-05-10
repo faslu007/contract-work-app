1f09484ab5158cfb19d77a51b3ee3e3ecd8b5790
faslu007 <faslu007@gmail.com>
2023-05-10 09:57:40 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,18 @@ const app = express();
 
 
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'), function (err) {
-        if (err) {
-            res.status(500).send(err);
-        }
-    });
-});
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
