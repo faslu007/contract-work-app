d789367e42e0ff00cb1d5b670b65029af8a96d6f
faslu007 <faslu007@gmail.com>
2023-05-10 09:52:20 +0530

    issue resolve

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'), function (err) {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'), function (err) {
         if (err) {
             res.status(500).send(err);
         }
